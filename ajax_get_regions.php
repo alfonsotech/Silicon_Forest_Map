1bf3e4801126cbf1d5fa794546ac54a1d3b80fc3
toddhb <tbrochu@pdx.edu>
2015-10-21 10:53:26 -0700

    Edited ajax_get_regions.php from mysql to mysqli syntax

--- ajax_get_regions.php
+++ ajax_get_regions.php
@@ -8,29 +8,32 @@ Copyright 2015 Todd Brochu
     <?php
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $dbhost = $url["host"];
-        $dbuser = $url["user"];
-        $dbpass = $url["pass"];
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
         $dbname = substr($url["path"], 1);
         /* include 'credentials.php'; */
         
         //Connect to MySQL server
-        mysql_connect($dbhost, $dbuser, $dbpass);
+        //mysql_connect($server, $username, $password);
+        $conn = new mysqli($server, $username, $password, $dbname);
 
         //Select database
-        mysql_select_db($dbname) or die(mysql_error());
+        //mysql_select_db($dbname) or die(mysql_error());
 
         //build query
-        $query = "SELECT DISTINCT region FROM Employers ORDER BY region";
+        $sql = "SELECT DISTINCT region FROM Employers ORDER BY region";
 
         //Execute query
-        $qry_result = mysql_query($query) or die(mysql_error());
+        //$qry_result = mysql_query($query) or die(mysql_error());
 
+        if ($result = $conn->query($sql)) {
         //Build Result String
         $display_string = "<ul><form action=\"\">";
 
         // Insert a new row in the table for each person returned
-        while($row = mysql_fetch_array($qry_result)){
+        //while($row = mysql_fetch_array($qry_result)){
+        foreach($result as $row) {
           $display_string .= "<input type=\"checkbox\" name=\"checkbox\" value=\"$row[region]\"      onclick=\"getCompaniesByRegion(value)\"> $row[region] (";
             
           //get the number of companies for each region
@@ -42,16 +45,20 @@ Copyright 2015 Todd Brochu
           $display_string .= ")<br>";
         }
         $display_string .= "<input type=\"checkbox\" id=\"checkall\" onclick=\"toggleAllMarkers()\"> {all of the above} (";
+            } else {
+            throw new Exception($conn->error);
+        }
 
           //get the number of companies for each region
-          $query = "SELECT COUNT(*) FROM Employers";
+          /*$query = "SELECT COUNT(*) FROM Employers";
           $qty_result = mysql_query($query) or die(mysql_error());
           $qty = mysql_fetch_row($qty_result);
-          $display_string .= $qty[0];
+          $display_string .= $qty[0];*/
 
         $display_string .= ")</form></ul>";
         echo $display_string;
-        mysql_close();
+        $conn->close();
+        //mysql_close();
     ?>
 </body>
 </html>
