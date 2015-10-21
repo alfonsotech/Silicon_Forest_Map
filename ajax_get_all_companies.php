da58cf667871b6b2747770adcc78f151a37dac8b
toddhb <tbrochu@pdx.edu>
2015-10-21 11:02:53 -0700

    Edited ajax_get_all_companies.php for mysqli syntax

--- ajax_get_all_companies.php
+++ ajax_get_all_companies.php
@@ -8,26 +8,32 @@ Copyright 2015 Todd Brochu
     <?php
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $dbhost = $url["host"];
-        $dbuser = $url["user"];
-        $dbpass = $url["pass"];
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
         $dbname = substr($url["path"], 1);
         /* include 'credentials.php'; */
         
-        mysql_connect($dbhost, $dbuser, $dbpass);
-        mysql_select_db($dbname) or die(mysql_error());
+        //mysql_connect($dbhost, $dbuser, $dbpass);
+        $conn = new mysqli($server, $username, $password, $dbname);
 
-        $query = "SELECT * FROM Employers ORDER BY name";
-        $qry_result = mysql_query($query) or die(mysql_error());
+        //mysql_select_db($dbname) or die(mysql_error());
 
+        $sql = "SELECT * FROM Employers ORDER BY name";
+        //$qry_result = mysql_query($query) or die(mysql_error());
+
+        if ($result = $conn->query($sql)) {
         $display_string = "[]";
 
-        while($row = mysql_fetch_array($qry_result)){
+        //while($row = mysql_fetch_array($qry_result)){
+        foreach($result as $row) {
             $display_string .= " ,[\"$row[id]\", \"$row[name]\", \"$row[latitude]\", \"$row[longitude]\", \"$row[address]\", \"$row[region]\", \"$row[url]\", \"$row[phone]\"]";
         }
          
         echo $display_string;
-        mysql_close();
+        }
+        $conn->close();
+        //mysql_close();
     ?>
 </body>
 </html>
